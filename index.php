93983cc50e8e34c5a432eafbe7f9c4b2702b5e76
efegabo <127172097+efegabo@users.noreply.github.com>
2023-05-03 02:01:59 -0500

    Update index.php

--- index.php
+++ index.php
@@ -28,7 +28,7 @@ include("foot.php")
 <script >  
 
 document.title="Gabriel suarez"; 
-document.getElementById("foto").src="./images/gabriel.png";
+document.getElementById("foto").src="./images/gabriel.jpg";
 document.getElementById("elnombre").innerHTML="Gabriel suarez";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
